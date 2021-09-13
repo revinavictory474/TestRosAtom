9ef6c869640be58fe0b62a071cfff2ebc8e9c96e
Revina Victory <revina.victory@gmail.com>
2021-09-13 14:48:08 +0700

    unity ui

---
+++ RusAtom/Assets/Code/UI/ButtonSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI
+{
+    public class ButtonSettings : MonoBehaviour
+    {
+        private void OpenSettingMenu()
+        {
+            
+        }
+    }
+}
