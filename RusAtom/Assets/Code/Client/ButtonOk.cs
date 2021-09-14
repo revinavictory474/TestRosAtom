a121497fd464d008aaed2685b0da2b5a89582d6c
Revina Victory <revina.victory@gmail.com>
2021-09-14 18:37:58 +0700

    client v1

---
+++ RusAtom/Assets/Code/Client/ButtonOk.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonOk : MonoBehaviour
+{
+    public Client client;
+    public Animator animator;
+
+    [SerializeField] private Text textIP;
+
+    private void Awake()
+    {
+        client = new Client();
+    }
+
+    public void OnMouseDown()
+    {
+        client.StartConnection();    
+    }
+
+    private void Update()
+    {
+        client.address = textIP.text;
+
+        if (client.message == "Start")
+        {
+            animator.SetBool("isAnimated", true);
+        }
+    }
+}
---
+++ RusAtom/Assets/Code/Client/ClientView.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClientView : MonoBehaviour
+{
+    
+    //private Client client;
+
+    //private void Awake()
+    //{
+    //    client = new Client();
+    //}
+    //private void Update()
+    //{
+    //    if(client.message == "Start")
+    //    {
+    //        Debug.Log("YEP");
+    //        animator.SetBool("a_Idle_Happy", true);
+    //    }
+    //}
+}
